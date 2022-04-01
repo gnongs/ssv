1c8166d70c93addd78aaeeae933d57492eb559b9
Alon Muroch <alonmuroch@gmail.com>
2022-04-01 09:59:45 +0300

    prev

--- docs/spec/ssv/spectest/alltests.go
+++ docs/spec/ssv/spectest/alltests.go
@@ -2,26 +2,27 @@ package spectest
 
 import (
 	tests2 "github.com/bloxapp/ssv/docs/spec/ssv/spectest/tests"
+	"github.com/bloxapp/ssv/docs/spec/ssv/spectest/tests/consensus"
 	"github.com/bloxapp/ssv/docs/spec/ssv/spectest/tests/postconsensus"
 )
 
 var AllTests = []*tests2.SpecTest{
-	//tests2.HappyFullFlow(),
-	//
-	//postconsensus.ValidMessage(),
-	//postconsensus.NoSigners(),
-	//postconsensus.InvaliSignature(),
-	//postconsensus.WrongSigningRoot(),
-	//postconsensus.WrongBeaconChainSig(),
-	//postconsensus.NoMessageSigners(),
-	//postconsensus.MultipleSigners(),
-	//postconsensus.MultipleMessageSigners(),
-	//postconsensus.FutureConsensusState(),
+	tests2.HappyFullFlow(),
+
+	postconsensus.ValidMessage(),
+	postconsensus.NoSigners(),
+	postconsensus.InvaliSignature(),
+	postconsensus.WrongSigningRoot(),
+	postconsensus.WrongBeaconChainSig(),
+	postconsensus.NoMessageSigners(),
+	postconsensus.MultipleSigners(),
+	postconsensus.MultipleMessageSigners(),
+	postconsensus.FutureConsensusState(),
 	postconsensus.PastConsensusState(),
 
-	//consensus.UnknownDuty(),
-	//consensus.WrongDutyRole(),
-	//consensus.WrongDutyPubKey(),
-	//consensus.DecidedValueSlotMismatch(),
-	//consensus.AttestationDataNil(),
+	consensus.UnknownDuty(),
+	consensus.WrongDutyRole(),
+	consensus.WrongDutyPubKey(),
+	consensus.DecidedValueSlotMismatch(),
+	consensus.AttestationDataNil(),
 }
--- docs/spec/ssv/spectest/tests/postconsensus/post_consensus_msg_after_reconstruction.go
+++ docs/spec/ssv/spectest/tests/postconsensus/post_consensus_msg_after_reconstruction.go
@@ -1,1 +1,8 @@
 package postconsensus
+
+import "github.com/bloxapp/ssv/docs/spec/ssv/spectest/tests"
+
+// MsgAfterReconstruction tests msg received after partial sig reconstructed and consensus state set to finished
+func MsgAfterReconstruction() *tests.SpecTest {
+	panic("implement")
+}
