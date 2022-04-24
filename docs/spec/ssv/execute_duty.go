1c630a649be013a96f6da51ce104699de1e2156b
Alon Muroch <alonmuroch@gmail.com>
2022-04-24 11:24:24 +0300

    WIP block proposal

--- docs/spec/ssv/execute_duty.go
+++ docs/spec/ssv/execute_duty.go
@@ -20,12 +20,29 @@ func (v *Validator) StartDuty(duty *beacon.Duty) error {
 	switch dutyRunner.BeaconRoleType {
 	case beacon.RoleTypeAttester:
 		return v.executeAttestationDuty(duty, dutyRunner)
+	case beacon.RoleTypeProposer:
+		return v.executeBlockProposalDuty(duty, dutyRunner)
 	default:
 		return errors.Errorf("duty type %s unkwon", duty.Type.String())
 	}
 	return nil
 }
 
+// executeBlockProposalDuty steps:
+// 1) sign a partial randao sig and wait for 2f+1 partial sigs from peers
+// 2) reconstruct randao and send GetBeaconBlock to BN
+// 3) start consensus on duty + block data
+// 4) Once consensus decides, sign partial block and broadcast
+// 5) collect 2f+1 partial sigs, reconstruct and broadcast valid block sig to the BN
+func (v *Validator) executeBlockProposalDuty(duty *beacon.Duty, dutyRunner *DutyRunner) error {
+
+}
+
+// executeAttestationDuty steps:
+// 1) get attestation data from BN
+// 2) start consensus on duty + attestation data
+// 3) Once consensus decides, sign partial attestation and broadcast
+// 4) collect 2f+1 partial sigs, reconstruct and broadcast valid attestation sig to the BN
 func (v *Validator) executeAttestationDuty(duty *beacon.Duty, dutyRunner *DutyRunner) error {
 	attData, err := v.beacon.GetAttestationData(duty.Slot, duty.CommitteeIndex)
 	if err != nil {
