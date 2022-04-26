97fe7fd993adca701d725f78a91de8d87707914c
Alon Muroch <alonmuroch@gmail.com>
2022-04-26 15:39:03 +0300

    remove to do

--- docs/spec/ssv/value_check.go
+++ docs/spec/ssv/value_check.go
@@ -15,11 +15,6 @@ func dutyValueCheck(duty *beacon.Duty, network BeaconNetwork) error {
 }
 
 func BeaconAttestationValueCheck(network BeaconNetwork) qbft.ProposedValueCheck {
-	// TODO - check for far future singning? https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/validator.md#protection-best-practices
-	// TODO - check attestation target and source for weird values (for example target in future epoch etc)
-	// TODO - check duty slot equal to attestation slot
-	// TODO - check duty committee index to attestation committee index
-
 	return func(data []byte) error {
 		cd := &types.ConsensusData{}
 		if err := cd.Decode(data); err != nil {
