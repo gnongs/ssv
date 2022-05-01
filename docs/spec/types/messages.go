ed5968da3a2389bee947f1e459c430d968496a4e
Alon Muroch <alonmuroch@gmail.com>
2022-05-01 10:52:12 +0300

    msg id

--- docs/spec/types/messages.go
+++ docs/spec/types/messages.go
@@ -1,13 +1,69 @@
 package types
 
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"github.com/bloxapp/ssv/beacon"
+)
+
 // ValidatorPK is an eth2 validator public key
 type ValidatorPK []byte
+
+const (
+	pubKeySize       = 48
+	pubKeyStartPos   = 0
+	roleTypeSize     = 4
+	roleTypeStartPos = pubKeyStartPos + pubKeySize
+)
 
 type Validate interface {
 	// Validate returns error if msg validation doesn't pass.
 	// Msg validation checks the msg, it's variables for validity.
 	Validate() error
 }
+
+// MessageIDBelongs returns true if message ID belongs to validator
+func (vid ValidatorPK) MessageIDBelongs(msgID MessageID) bool {
+	toMatch := msgID.GetPubKey()
+	return bytes.Equal(vid, toMatch)
+}
+
+// MessageID is used to identify and route messages to the right validator and Runner
+type MessageID []byte
+
+func (msg MessageID) GetPubKey() []byte {
+	return msg[pubKeyStartPos : pubKeyStartPos+pubKeySize]
+}
+
+func (msg MessageID) GetRoleType() beacon.RoleType {
+	roleByts := msg[roleTypeStartPos : roleTypeStartPos+roleTypeSize]
+	return beacon.RoleType(binary.LittleEndian.Uint32(roleByts))
+}
+
+func NewMsgID(pk []byte, role beacon.RoleType) MessageID {
+	roleByts := make([]byte, 4)
+	binary.LittleEndian.PutUint32(roleByts, uint32(role))
+	return append(pk, roleByts...)
+}
+
+func (msgID MessageID) String() string {
+	return hex.EncodeToString(msgID)
+}
+
+type MsgType uint64
+
+const (
+	// SSVConsensusMsgType are all QBFT consensus related messages
+	SSVConsensusMsgType MsgType = iota
+	// SSVDecidedMsgType are all QBFT decided messages
+	SSVDecidedMsgType
+	// SSVSyncMsgType are all QBFT sync messages
+	SSVSyncMsgType
+	// SSVPartialSignatureMsgType are all partial signatures msgs over beacon chain specific signatures
+	SSVPartialSignatureMsgType
+)
 
 type Root interface {
 	// GetRoot returns the root used for signing and verification
---
+++ docs/spec/types/testingutils/runner.go
@@ -0,0 +1,107 @@
+package testingutils
+
+import (
+	"github.com/bloxapp/ssv/beacon"
+	"github.com/bloxapp/ssv/docs/spec/qbft"
+	"github.com/bloxapp/ssv/docs/spec/ssv"
+	"github.com/bloxapp/ssv/docs/spec/types"
+)
+
+var AttesterRunner = func() *ssv.Runner {
+	return baseRunner(beacon.RoleTypeAttester, ssv.BeaconAttestationValueCheck(NewTestingKeyManager(), ssv.NowTestNetwork))
+}
+
+var ProposerRunner = func() *ssv.Runner {
+	return baseRunner(beacon.RoleTypeProposer, ssv.BeaconBlockValueCheck(NewTestingKeyManager(), ssv.NowTestNetwork))
+}
+
+var baseRunner = func(role beacon.RoleType, valCheck qbft.ProposedValueCheck) *ssv.Runner {
+	return ssv.NewDutyRunner(
+		role,
+		ssv.NowTestNetwork,
+		TestingShare,
+		NewTestingQBFTController(types.NewMsgID(TestingValidatorPubKey[:], role), valCheck),
+		NewTestingStorage(),
+		valCheck,
+	)
+}
+
+var DecidedRunner = func() *ssv.Runner {
+	return decideRunner(TestAttesterConsensusDataByts, qbft.FirstHeight)
+}
+
+var DecidedRunnerWithHeight = func(height qbft.Height) *ssv.Runner {
+	return decideRunner(TestAttesterConsensusDataByts, height)
+}
+
+var DecidedRunnerUnknownDutyType = func() *ssv.Runner {
+	return decideRunner(TestConsensusUnkownDutyTypeDataByts, qbft.FirstHeight)
+}
+
+var decideRunner = func(consensusData []byte, height qbft.Height) *ssv.Runner {
+	v := BaseValidator()
+	for h := qbft.Height(qbft.FirstHeight); h <= height; h++ {
+		msgs := []*types.SSVMessage{
+			SSVMsgAttester(SignQBFTMsg(TestingSK1, 1, &qbft.Message{
+				MsgType:    qbft.ProposalMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       ProposalDataBytes(consensusData, nil, nil),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK1, 1, &qbft.Message{
+				MsgType:    qbft.PrepareMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       PrepareDataBytes(consensusData),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK2, 2, &qbft.Message{
+				MsgType:    qbft.PrepareMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       PrepareDataBytes(consensusData),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK3, 3, &qbft.Message{
+				MsgType:    qbft.PrepareMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       PrepareDataBytes(consensusData),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK1, 1, &qbft.Message{
+				MsgType:    qbft.CommitMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       CommitDataBytes(consensusData),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK2, 2, &qbft.Message{
+				MsgType:    qbft.CommitMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       CommitDataBytes(consensusData),
+			}), nil),
+			SSVMsgAttester(SignQBFTMsg(TestingSK3, 3, &qbft.Message{
+				MsgType:    qbft.CommitMsgType,
+				Height:     h,
+				Round:      qbft.FirstRound,
+				Identifier: []byte{1, 2, 3, 4},
+				Data:       CommitDataBytes(consensusData),
+			}), nil),
+		}
+
+		if err := v.DutyRunners[beacon.RoleTypeAttester].Decide(TestAttesterConsensusData); err != nil {
+			panic(err.Error())
+		}
+		for _, msg := range msgs {
+			if err := v.ProcessMessage(msg); err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+
+	return v.DutyRunners[beacon.RoleTypeAttester]
+}
